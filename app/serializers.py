363cedea8d7fa7897981ef5246f78d9d27d01c9f
fanni <fmalevych@zoolatech.com>
2025-03-04 08:05:36 +0200

    extract serializers

---
+++ app/serializers.py
@@ -0,0 +1,24 @@
+import json
+from xml.etree import ElementTree
+from abc import ABC, abstractmethod
+
+
+class Serializer(ABC):
+    @abstractmethod
+    def serialize(self, title: str, content: str) -> str:
+        pass
+
+
+class JsonSerializer(Serializer):
+    def serialize(self, title: str, content: str) -> str:
+        return json.dumps({"title": title, "content": content})
+
+
+class XmlSerializer(Serializer):
+    def serialize(self, title: str, content: str) -> str:
+        root = ElementTree.Element("book")
+        title_element = ElementTree.SubElement(root, "title")
+        title_element.text = title
+        content_element = ElementTree.SubElement(root, "content")
+        content_element.text = content
+        return ElementTree.tostring(root, encoding="unicode")
